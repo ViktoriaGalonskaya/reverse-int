8fb4007c20b26f62e293fff9d3d8642b4e9eeab4
victoriagalonskaya <victoriagalonskaya1997@mail.ru>
2023-01-08 18:33:33 +0800

    feat: done reverse-int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+  const changeString= Math.abs(n).toString();
+  let newString = '';
+  for (let i = changeString.length - 1; i >= 0; i--){
+   newString = newString + changeString[i];
+  } 
+  return +newString;
 }
